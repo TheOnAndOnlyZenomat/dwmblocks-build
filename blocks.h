f66a7eeacebcfb8968a36d73b75b2b508914059e
TheOnAndOnlyZenomat <eichelstein@online.de>
2020-07-28 14:05:18 +0200

    changed delimiter

--- blocks.h
+++ blocks.h
@@ -7,24 +7,22 @@ static const Block blocks[] = {
 	
 	{"", "/home/adrian/scripts/statusbar/cpu.sh",				1,		0},
 	
+	{"", "/home/adrian/scripts/statusbar/temp.sh",				5,		0},
+
 	{"", "/home/adrian/scripts/statusbar/ram.sh",				5,		0},
 
 	{"", "/home/adrian/scripts/statusbar/uptime.sh",			60,		0},
 
 	{"", "/home/adrian/scripts/statusbar/volume.sh",			0,		11},
 
-	{"", "/home/adrian/scripts/statusbar/temp.sh",				5,		0},
-
 	{"", "/home/adrian/scripts/statusbar/new_weather.sh",			60,		0},
 
 	{"", "/home/adrian/scripts/statusbar/clock.sh",				1,		0},
 	
 	{"", "/home/adrian/scripts/statusbar/upgrades.sh",			30,		13},
-
-	{"", "/home/adrian/scripts/statusbar/installdate.sh",			0,		15},
 	
 	{"", "/home/adrian/scripts/statusbar/posture.sh",			1,		16},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim = '|';
+static char delim = ' ';
